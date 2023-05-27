653d5ea31dacf35523a287d13234fe815e44e6f5
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-05-27 15:18:16 -0400

    Added global.h

---
+++ include/global.h
@@ -0,0 +1,14 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#include <tmmintrin.h> // SSSE3 intrinsics
+#include <algorithm>
+#include <iostream>
+#include <unistd.h>
+#include <tgmath.h> 
+#include <iomanip>
+#include <complex>
+#include <string>
+#include <vector>
+
+#endif
