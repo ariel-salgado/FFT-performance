bdcb39cb86c6ddd9de86d8288825499f824b3304
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-05-28 16:47:34 -0400

    printResult() is now a private method

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,17 @@ int main(int argc, char* argv[]) {
 
     int32_t SIZE;
     bool    SIMD;
+    bool    PRINT;
 
     checkArgs* args = new checkArgs(argc, argv);
 
-	SIZE = args->getArgs().SIZE;
-    SIMD = args->getArgs().SIMD;
+	SIZE  = args->getArgs().SIZE;
+    SIMD  = args->getArgs().SIMD;
+    PRINT = args->getArgs().PRINT;
 
-    Wave* wave = new Wave(SIZE, SIMD);
+    Wave* wave = new Wave(SIZE, SIMD, PRINT);
 
     wave->performFFT();
-    wave->printResults();
 
     return EXIT_SUCCESS;
 }
